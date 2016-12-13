b355b63a93ab44f187ad8dd2d3bf57f391cbb706
Jingjun Zhang <jjnzhang@CNjjnzhang.local>
2016-12-14 05:18:59 +0800

    Add main method

--- src/main/java/com/thoughtworks/FizzBuzz/Core.java
+++ src/main/java/com/thoughtworks/FizzBuzz/Core.java
@@ -4,6 +4,17 @@ package com.thoughtworks.FizzBuzz;
  * Created by jjnzhang on 12/14/16.
  */
 public class Core {
+    public static void main(String[] args) {
+        int max = 100;
+        Core core = new Core();
+        Convert convert = new Convert(core);
+        String[] numberDescriptions = convert.convertToNumberDescriptions(max);
+
+        for (String numberDescription : numberDescriptions) {
+            System.out.println(numberDescription);
+        }
+    }
+
     public String convertToNumberDescription(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
