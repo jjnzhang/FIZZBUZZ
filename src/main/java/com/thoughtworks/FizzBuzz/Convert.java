ad0f87f3867209ac4246742aca7d658ad2c25cdb
Jingjun Zhang <jjnzhang@CNjjnzhang.local>
2016-12-14 05:09:43 +0800

    should_return_empty_given_0 succeed

--- src/main/java/com/thoughtworks/FizzBuzz/Convert.java
+++ src/main/java/com/thoughtworks/FizzBuzz/Convert.java
@@ -13,6 +13,12 @@ public class Convert {
 
 
     public String[] convertToNumberDescriptions(int max) {
-        return null;
+        String[] result = new String[max];
+        for (int index = 0; index < max; index++) {
+            result[index] =
+                    core.convertToNumberDescription(index + 1);
+        }
+
+        return result;
     }
 }
